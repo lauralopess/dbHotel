287af3c6003df63dd3a8c1513890b0013dcb30d9
lauralopess <lauralopessts@gmail.com>
2023-10-19 12:05:45 -0300

    Segunda versão

--- scriptMySQL.sql
+++ scriptMySQL.sql
@@ -11,6 +11,20 @@ use dbHotel;
 /* Excluir a tabela do banco */
 drop table funcionarios;
 
+/* Adicionar o campo email à tabela funcionarios */
+alter table funcionarios add column email varchar(50);
+
+alter table funcionarios add column nomeFunc varchar(100) not null after idFunc;
+
+/* Excluir um campo da tabela */
+alter table funcionarios drop column email;
+
+/* Reposicionar o campo email para que ele fique após o campo login */
+alter table funcionarios modify column email varchar(50) not null after login;
+
+/* Modificar o campo email para que se torne obrigatório, ou seja, não nulo (not null) */
+alter table funcionarios modify column email varchar(50) not null;
+
 /* Criar uma nova tabela */
 create table funcionarios (
 /* Int se refere ao tipo do campo na tabela
@@ -37,25 +51,30 @@ show tables;
 
 /* CREATE */
 /* Inserir um novo funcionário na tabela, com nome, login, senha criptografada e cargo */
-insert into funcionarios(nomeFunc, login, senha, cargo) values ("Administrador", "admin", md5("admin"), "Administrador");
+insert into funcionarios(nomeFunc, login, senha, cargo, email) values ("Administrador", "admin", md5("admin"), "Administrador", "admin@gmail.com");
 
-insert into funcionarios(nomeFunc, login, senha, cargo) values ("Pamella Pereto", "pamellapereto", md5("123@senac"), "TI");
+insert into funcionarios(nomeFunc, login, senha, cargo, email) values ("Pamella Pereto", "pamellapereto", md5("123@senac"), "TI", "pamellapereto@gmail.com");
 
-insert into funcionarios(nomeFunc, login, senha, cargo) values ("Breno Silva", "brenosilva", md5("123@senac"), "Contabilidade");
+insert into funcionarios(nomeFunc, login, senha, cargo, email) values ("Breno Silva", "brenosilva", md5("123@senac"), "Contabilidade", "brenosilva@gmai.com");
 
-insert into funcionarios(nomeFunc, login, senha, cargo) values ("Victoria Cardoso", "victoriacardoso", md5("123@senac"), "RH");
+insert into funcionarios(nomeFunc, login, senha, cargo, email) values ("Victoria Cardoso", "victoriacardoso", md5("123@senac"), "RH", "victoriacardoso@gmail.com");
 
-insert into funcionarios(nomeFunc, login, senha) values ("Laura Lopes", "lauralopes", md5("123@senac"));
+insert into funcionarios(nomeFunc, login, senha, email) values ("Laura Lopes", "lauralopes", md5("123@senac"), "lauralopes@gmail.com");
 
-insert into funcionarios(nomeFunc, login, senha) values ("Felipe Coelho", "felipe", md5("123@senac"));
+insert into funcionarios(nomeFunc, login, senha, email) values ("Felipe Coelho", "felipe", md5("123@senac"), "felipecoelho@gmail.com");
 
 
 /* READ */
 /* Ler/Buscar as informações da tabela funcionários */
 select * from funcionarios;
 
+/* Buscar o login e a senha da tabela funcionarios em que o login seja admin e senha seja admin */
+select login as Login, senha from funcionarios where login = "admin" and senha =md5("admin");
+
 
 /* UPDATE */
 update funcionarios set login = "felipecoelho" where idFunc = 8;
 
 update funcionarios set cargo = "Gerência" where idFunc = 5;
+
+/* update funcionarios set email = "lauralopes@gmail.com" where idFunc = 5; */
